d4e92a4a2e88a86162cd719095150579000c8895
Satyam1722 <72600713+Satyam1722@users.noreply.github.com>
2023-01-03 10:03:06 +0530

    3 janurary first solution

---
+++ 3jan_1.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+         vector<vector<int>> v(numRows);
+         for(int i=0;i<numRows;i++)
+         {
+             for(int j=0;j<=i;j++)
+            {
+                if(j==0||j==i)
+                v[i].push_back(1);
+                else
+                v[i].push_back(v[i-1][j]+v[i-1][j-1]);
+            }
+         }  
+         return v;
+    }
+};
